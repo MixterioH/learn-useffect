bf1f71b9ab7ea496c36fb44401c70ca370ae1d7c
MixterioH <hende9canal@gmail.com>
2023-10-05 15:30:46 -0300

    feat: finish app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useEffect, useState } from "react"
+import userdefault from "./userdefault.jpg"
 
 function App() {
+  const [user, setUser] = useState()
+  const [searchUser, setSearchUser] = useState()
+  const [searchedUser, setSearchedUser] = useState()
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    setSearchedUser(searchUser)
+  }
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${searchedUser}`)
+      .then((res) => res.json())
+      .then((json) => setUser(json))
+  }, [searchedUser])
+
+  let userIMG
+  if (searchedUser === undefined || searchedUser == "") {
+    userIMG = userdefault
+  } else {
+    userIMG = user.avatar_url
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {user && (
+        <div className="App">
+          <div>
+            <form onSubmit={handleFormSubmit}>
+              <input
+                type="text"
+                value={searchUser}
+                onChange={(e) => setSearchUser(e.target.value)}
+                placeholder="User Name in Github"
+              />
+              <button type="submit">Pesquisar</button>
+            </form>
+
+            <h1>O usuário que você digitou é: </h1>
+
+            <h2>{user.name}</h2>
+
+            <img src={userIMG} />
+            <p>Mora em: {user.location}</p>
+            <p>Bio: {user.bio}</p>
+
+            <a href={user.html_url} target="_blank">
+              Link para o perfil
+            </a>
+          </div>
+        </div>
+      )}
+    </>
+  )
 }
 
-export default App;
+export default App
